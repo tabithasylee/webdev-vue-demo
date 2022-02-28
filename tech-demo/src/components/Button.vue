1d93d1f438160ff50256c013ec08284eb744ef84
Tabitha Lee <tabitha.see.ya.lee@vanderbilt.edu>
2022-02-28 01:42:15 -0600

    initial

---
+++ tech-demo/src/components/Button.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  count: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <button @click="count++">{{count}}</button>
+</template>
+
+<style scoped>
+button {
+    background-color:green;
+    padding: 1em;
+}
+</style>
---
+++ tech-demo/src/views/CountView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Button from '@/components/Button.vue'
+
+</script>
+
+<template>
+  <main>
+    <Button count=1></Button>
+  </main>
+</template>
